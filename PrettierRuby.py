e021f812ff104652d2c8cd638d07e4ec477684b0
Marlen Brunner <klondikemarlen@gmail.com>
2022-05-02 11:55:06 -0700

    :construction: Closer to working rbprettier on auto-save.

--- PrettierRuby.py
+++ PrettierRuby.py
@@ -1,10 +1,79 @@
+import os
+from subprocess import Popen, PIPE
+
 import sublime
 import sublime_plugin
 
+from .prettier_ruby.utils import (
+    decode_bytes,
+    format_error_message,
+    get_proc_env,
+    is_windows,
+    normalize_line_endings,
+    resolve_path,
+)
 
-class PrettierRubyCommand(sublime_plugin.ViewEventListener):
-	# on_post_save_async(view)
-	def on_pre_save(self):
-		print("self.view.file_name()", self.view.file_name())
-		file_name = self.view.file_name()
 
+class PrettierRubyCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        source_file_path = view.file_name()
+
+        region = sublime.Region(0, view.size())
+        source_text = view.substr(region)
+
+        prettified_text = self.format_code(source_text)
+
+        view.replace(edit, region, prettified_text)
+
+    def format_code(self, source_text):
+        source_file_path = self.view.file_name()
+        node_cmd = "node.exe" if is_windows() else "node"
+        node_path = resolve_path(
+            node_cmd, source_file_path, default="/home/marlen/.asdf/shims/node"
+        )
+        rbprettier_cli_path = resolve_path(
+            "rbprettier",
+            source_file_path,
+            default="/home/marlen/.asdf/shims/rbprettier",
+        )
+        cmd = [
+            node_path,
+            rbprettier_cli_path,
+        ]
+        try:
+            proc = Popen(
+                cmd,
+                stdin=PIPE,
+                stderr=PIPE,
+                stdout=PIPE,
+                env=get_proc_env(),
+                shell=is_windows(),
+            )
+            stdout, stderr = proc.communicate(input=source_text.encode("utf-8"))
+
+            if stderr:
+                stderr_output = normalize_line_endings(decode_bytes(stderr))
+                if stderr_output:
+                    print(format_error_message(stderr_output, str(proc.returncode)))
+
+            return normalize_line_endings(decode_bytes(stdout))
+        except OSError as ex:
+            sublime.error_message(
+                "{plugin_name} - {error}".format(plugin_name="PrettierRuby", error=ex)
+            )
+            raise
+
+
+class CommandOnSave(sublime_plugin.ViewEventListener):
+    def on_pre_save(self):
+        view = self.view
+        source_file_path = view.file_name()
+        _, source_file_name = os.path.split(source_file_path)
+        root, extension = os.path.splitext(source_file_name)
+        extension = root if extension == "" else extension
+
+        if extension not in [".rb", ".gemspec", "Gemfile"]:
+            return
+
+        self.view.run_command("prettier_ruby")
---
+++ prettier_ruby/utils.py
@@ -0,0 +1,125 @@
+import locale
+import os
+import platform
+import shutil
+
+
+def is_windows():
+    return platform.system() == "Windows" or os.name == "nt"
+
+
+def decode_bytes(bytes_to_decode):
+    """
+    Decode and return a byte string using utf-8, falling back to system's encoding if that fails.
+
+    Source: Sublime Linter
+    https://github.com/SublimeLinter/SublimeLinter/blob/master/lint/util.py#L272
+    """
+    if not bytes_to_decode:
+        return ""
+
+    try:
+        return bytes_to_decode.decode("utf-8")
+    except UnicodeError:
+        return bytes_to_decode.decode(locale.getpreferredencoding(), errors="replace")
+
+
+def format_error_message(error_message, error_code):
+    # inject a line break between the error message, and debug output (legibility purposes):
+    error_message = error_message.replace("[error] stdin: ", "\n[error] stdin: ")
+
+    return (
+        "\nPrettier reported the following output:\n\n"
+        "{0}\n"
+        "\nPrettier process finished with exit code {1}.\n".format(
+            error_message, "{0}".format(error_code)
+        )
+    )
+
+
+def env_path_contains(path_to_look_for, env_path=None):
+    """Check if the specified path is listed in OS environment path.
+
+    :param path_to_look_for: The path the search for.
+    :param env_path: The environment path str.
+    :return: True if the find_path exists in the env_path.
+    :rtype: bool
+    """
+    if not path_to_look_for:
+        return False
+    if not env_path:
+        env_path = os.environ["PATH"]
+    path_to_look_for = str.replace(path_to_look_for, os.pathsep, "")
+    paths = env_path.split(os.pathsep)
+    for path in paths:
+        if path == path_to_look_for:
+            return True
+    return False
+
+
+def env_path_exists(path):
+    if not path:
+        return False
+    if os.path.exists(str.replace(path, os.pathsep, "")):
+        return True
+    return False
+
+
+def get_proc_env():
+    env = None
+    if not is_windows():
+        env = os.environ.copy()
+        usr_path = ":/usr/local/bin"
+        if not env_path_contains(usr_path) and env_path_exists(usr_path):
+            env["PATH"] += usr_path
+    return env
+
+
+def normalize_line_endings(lines):
+    if not lines:
+        return lines
+    return lines.replace("\r\n", "\n").replace("\r", "\n")
+
+
+def ensure_file_has_ext(file_name, file_ext):
+    if not file_name.endswith(file_ext):
+        return "{0}{1}".format(file_name, file_ext)
+    return file_name
+
+
+def is_str_none_or_empty(val):
+    """Determine if the specified str val is None or an empty.
+
+    :param val: The str to check.
+    :return: True if if val: is None or an empty, otherwise False.
+    :rtype: bool
+    """
+    if val is None:
+        return True
+    if isinstance(val, string_types):
+        val = val.strip()
+    if not val:
+        return True
+    return False
+
+
+def which(command, source_file=None):
+    path = os.defpath
+    if source_file:
+        search_paths = source_file.split(os.pathsep)
+        path = ":".join(search_paths)
+
+    return shutil.which(command, path)
+
+
+def resolve_path(command, source_file, default=None):
+    command_path = which(command, source_file)
+    if command_path:
+        return command_path
+
+    if default:
+        return default
+
+    raise EnvironmentError(
+        "Could not determine path for command: {command}".format(command=command)
+    )
